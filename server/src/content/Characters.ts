7b78469edf72874aeb0ec9993fb4eec8c1760d68
Lai, Johnnie <johnnie.lai@vancouver.ca>
2023-11-01 12:30:48 -0700

    Added business path

--- server/src/content/Characters.ts
+++ server/src/content/Characters.ts
@@ -2,7 +2,8 @@ import type { CustomCharacter } from './types'
 
 import { lawyerCustom } from '../../config/lawyerCustom'
 import { studentCustom } from '../../config/studentCustom'
+import { businessCustom } from '../../config/businessCustom'
 
-const characters: CustomCharacter[] = [studentCustom, lawyerCustom]
+const characters: CustomCharacter[] = [studentCustom, lawyerCustom, businessCustom]
 
 export default characters
